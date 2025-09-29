30deae6915f8a547e947b8e1e41a58482d519c72
Valentinedarks <valentinerusnak@gmail.com>
2025-09-29 13:55:33 +0300

    Prikolyamba_2: dorobyv task_1 i dobavyv pidtrymku inshih mov

--- Prikolyamba_2.cpp
+++ Prikolyamba_2.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <cmath>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 using namespace std;
 
-void task_1() {
-    double a, f;
-    cout<<"Vvedit znachenya A="<<endl;
-    cin>>a;
-    f=a*f;
+void set_ua_locale() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
+    setlocale(LC_ALL, "uk_UA.UTF-8");
 }
 
+void task_1() {
+    set_ua_locale();
+    double a, f;
+    cout<<"Введіть значення А="<<endl;
+    cin>>a;
+    if (a<-2) {
+        f=sin(a)+fabs(a);
+    } else {
+        f=cbrt(a);
+    }
+    cout<<"f(a)="<<f<<endl;
+
+}
+
+void task_2() {
+    int x,f;
+    cout<<"Введіть значення х="<<endl;
+    cin>>x;
+    //Залишу цю задачу до кращих часів
+}
 
 void task_3() {
+    set_ua_locale();
     double a, b, c;
-    cout<<"Vvedit znachenya="<<endl;
+    cout<<"Введіть значення="<<endl;
     cin>>a>>b>>c;
     if (a+b>c && a+c>b && b+c>a) {
-        cout<<"Mozhna pobuduvaty"<<endl;
+        cout<<"Можна побудувати"<<endl;
     }
     else
-        cout<<"Ne mozhlyvo pobuduvaty"<<endl;
+        cout<<"Трикутник неможливо побудувати"<<endl;
 }
 
 void task_5() {
+    set_ua_locale();
     int biletik;
-    cout<<"Vvedit shestiznachne chislo="<<endl;
+    cout<<"Введіть шестизначне число="<<endl;
     cin>>biletik;
     int q1=biletik/100000;
     int q2=(biletik/10000)%10;
@@ -35,42 +64,44 @@ void task_5() {
     int sum1=q1+q2+q3;
     int sum2=q4+q5+q6;
     if (sum1==sum2) {
-        cout<<"Kvitok pizdatiy :3"<<endl;
+        cout<<"Квиток щасливий :3"<<endl;
     }
     else
-        cout<<"Pizda tobi :p"<<endl;
+        cout<<"Пизда тобі :p"<<endl;
 }
 
 void task_6() {
+    set_ua_locale();
     int a;
-    cout<<"Vvedit kilkist baliv="<<endl;
+    cout<<"Введіть кількість балів="<<endl;
     cin>>a;
     if(a<=74 && a>=50) {
-        cout<<"Mark E"<<endl;
+        cout<<"Оцінка E"<<endl;
     }
     else if(a>74 && a<=81) {
-        cout<<"Mark D"<<endl;
+        cout<<"Оцінка D"<<endl;
     }
     else if(a>81 && a<=85) {
-        cout<<"Mark C"<<endl;
+        cout<<"Оцінка C"<<endl;
     }
     else if(a>85 && a<=91) {
-        cout<<"Mark B"<<endl;
+        cout<<"Оцінка B"<<endl;
     }
     else if(a>91 && a<=100) {
-        cout<<"Mark A"<<endl;
+        cout<<"Оцінка A"<<endl;
     }
     else if(a<50) {
-        cout<<"You stupid nigger :p"<<endl;
+        cout<<"На перездачу :p"<<endl;
     }
     else if(a>100) {
-        cout<<"ty spidoznaya kakashka :p"<<endl;
+        cout<<"Перечитай правила :p"<<endl;
     };
 }
 
 void task_7() {
+    set_ua_locale();
     int a,b,c,middle;
-    cout<<"Vvedit try znachenya="<<endl;
+    cout<<"Введіть три значення="<<endl;
     cin>>a>>b>>c;
     if ((a>=b && a<=c) || (a<=b && a>=c)) {
         middle=a;
@@ -83,10 +114,12 @@ void task_7() {
     cout<<middle<<endl;
 }
 int main() {
+    set_ua_locale();
     task_1();
-    task_3();
-    task_5();
-    task_6();
-    task_7();
+    task_2();
+    // task_3();
+    // task_5();
+    // task_6();
+    // task_7();
     return 0;
 }
